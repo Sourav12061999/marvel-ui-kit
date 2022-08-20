3295b464ded324fb1ded3cfb9d34496841440273
Sourav12061999 <sdunity12061999@gmail.com>
2022-08-20 19:17:32 +0530

    Input Component Complete

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,6 +11,18 @@ interface PropTypes extends HTMLAttributes<HTMLInputElement> {
   startComponent?: ReactNode;
   endComponent?: ReactNode;
   required?: boolean;
+  isError?: boolean;
+  errorMsg?: string;
+  ClassName?:ClassNameTypes;
+}
+
+interface ClassNameTypes {
+  inputClassName?:string;
+  containerClassName?:string;
+  inputContainerClassName?:string;
+  startComponentClassName?:string;
+  endComponentClassName?:string;
+  errorMSGClassName?:string;
 }
 function Input({
   type,
@@ -22,41 +34,45 @@ function Input({
   endComponent,
   required,
   label,
+  isError,
+  errorMsg,
   ...htmlAttributes
 }: PropTypes) {
   const containerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div
-      ref={containerRef}
-      id="container-id"
-      className={`${containerClassName}`}
-    >
-      <div>{startComponent}</div>
-      <div>
-        <label htmlFor="Input">
-          {`${label || ""}`} {required ? <span>*</span> : null}
-        </label>
-        <input
-          placeholder={placeholder}
-          type={type || "text"}
-          {...htmlAttributes}
-          className={`${className}`}
-          onFocus={() => {
-            if (containerRef.current != null) {
-              containerRef.current.style.boxShadow =
-                "0 0 5px rgba(81, 203, 238, 1)";
-            }
-          }}
-          onBlur={() => {
-            if (containerRef.current != null) {
-              containerRef.current.style.boxShadow =
-                "rgba(0, 0, 0, 0.05) 0px 0px 0px 1px";
-            }
-          }}
-        />
+    <div id="container-id" className={`${containerClassName}`}>
+      {label ? <label htmlFor="Input">{label}</label> : null}
+      {required ? <span>*</span> : null}
+      <div ref={containerRef}>
+        {startComponent && (
+          <div data-component-type="start">{startComponent}</div>
+        )}
+        <div className="input-container">
+          <input
+            placeholder={placeholder}
+            type={type || "text"}
+            {...htmlAttributes}
+            className={`${className}`}
+            onFocus={() => {
+              if (containerRef.current != null && !isError) {
+                containerRef.current.style.boxShadow =
+                  "0 0 5px rgba(81, 203, 238, 1)";
+              } else if (containerRef.current != null && isError) {
+                containerRef.current.style.boxShadow = "0 0 5px #f44336";
+              }
+            }}
+            onBlur={() => {
+              if (containerRef.current != null) {
+                containerRef.current.style.boxShadow =
+                  "rgba(0, 0, 0, 0.05) 0px 0px 0px 1px";
+              }
+            }}
+          />
+        </div>
+        {endComponent && <div data-component-type="end">{endComponent}</div>}
       </div>
-      <div>{endComponent}</div>
+      {isError && <small>{errorMsg}</small>}
     </div>
   );
 }
--- stories/Input/Input.stories.tsx
+++ stories/Input/Input.stories.tsx
@@ -1,12 +1,51 @@
 import { Input } from "../../src";
 import React from "react";
-
+import { BsFillTelephoneFill } from "react-icons/bs";
 export default {
-    title: "Input",
-    component: Input,
+  title: "Input",
+  component: Input,
 };
-  
 
-export const Default = () => (
-    <Input placeholder="Very Important"/>
-)
+export const Default = () => <Input placeholder="Very Important" />;
+
+export const StartIcon = () => (
+  <Input
+    startComponent={<BsFillTelephoneFill size={"20px"} />}
+    placeholder="Very Important"
+  />
+);
+
+export const EndIcon = () => (
+    <Input
+      endComponent={<BsFillTelephoneFill size={"20px"} />}
+      placeholder="Very Important"
+    />
+);
+
+export const Label = () => (
+    <Input
+    label="Name:-"
+      endComponent={<BsFillTelephoneFill size={"20px"} />}
+      placeholder="Very Important"
+    />
+);
+
+export const Required = () => (
+  <Input
+  label="Name:-"
+  required={true}
+    endComponent={<BsFillTelephoneFill size={"20px"} />}
+    placeholder="Very Important"
+  />
+);
+
+export const Error = () => (
+  <Input
+  label="Name:-"
+  required={true}
+    endComponent={<BsFillTelephoneFill size={"20px"} />}
+    placeholder="Very Important"
+    isError={true}
+    errorMsg="Some error occoured"
+  />
+);
